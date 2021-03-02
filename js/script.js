25a2ef1b6e12966c251ba8b05830acb83434c779
Alexandr <tolstov-01@mail.ru>
2021-03-02 17:08:09 +0300

    Add 2nd part of script

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@ let arr=['1','2','3','4','5']
 let i=1
 console.log(arr)
 let e=document.querySelector('img')
+function nextSlides () {
+    i++			
+    if(i>arr.length){
+        i=1
+    }
+    e.src=i+'.png'
+}
+function prevSlides (){
+    i--
+    if(i===0){
+        i=arr.length
+    }
+    e.src=i+'.png'
+}
